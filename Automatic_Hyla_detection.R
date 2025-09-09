d5087180aa1ae9d65fb01e900e69cc1a57f09d4f
MatiasIMunoz <ma.munozsandoval@gmail.com>
2025-09-09 11:45:55 -0500

    Added "Hyla_test.wav" audio file to repository. Started modifying scrit to load audio files.

--- Automatic_Hyla_detection.R
+++ Automatic_Hyla_detection.R
@@ -1,3 +1,30 @@
 ## R project for the automatic detection of Hyla sp. calls in R.
 ## Script will be used to analyze the data from the field season 2025.
 
+# Install packages if necessary. ----
+install.packages("seewave")
+install.packages("tuneR")
+install.packages("scales")
+install.packages("shiny")
+install.packages("knitr")
+
+# Load libraries. ----
+library("seewave")
+library("tuneR")
+library("scales")
+
+# Colorblind friendly colors. ----
+blue_col <- "#004488"
+red_col <- "#BB5566"
+yellow_col <- "#DDAA33"
+
+# Folder with .wav files. ----
+wav_folder <- "/audio_files/"
+
+# Check .wav files in folder. ----
+list.files(path = paste0(getwd(), wav_folder), pattern = "\\.wav$") #Check the presence of  WAVs in the working directory.
+
+# Load the "Hyla_test.wav" file. ----
+wav <- readWave(paste0(getwd(), wav_folder)) #requires package 'tuneR'.
+wav
+
